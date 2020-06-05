745171940875f33c96332790ef5ebff2efa27a91
Sören Jensen <sirn@sirn.se>
2020-06-05 19:24:18 +0200

    Error code

--- lib/ConnectionException.php
+++ lib/ConnectionException.php
@@ -5,7 +5,7 @@ namespace WebSocket;
 class ConnectionException extends Exception
 {
     // Native codes in interval 0-106
-    public const TIMED_OUT = 1024;
-    public const EOF = 1025;
-    public const BAD_OPCODE = 1026;
+    public static $TIMED_OUT = 1024;
+    public static $EOF = 1025;
+    public static $BAD_OPCODE = 1026;
 }
